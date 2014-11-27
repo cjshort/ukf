4e330fea1f17efe545bef9a15bcae65556ec2110
cjshort <callumshorty@hotmail.com>
2014-11-27 10:12:46 +0000

    http fix

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,4 +1,6 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+if Rails.env.development?
+	OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
 Gibbon::API.api_key = "5dbfa9b02dd8a28aa43937d19855da5e-us9"
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
