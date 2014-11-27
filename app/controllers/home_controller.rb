76720f61753946ebbbfc2f7dcea96012d2946fb5
cjshort <callumshorty@hotmail.com>
2014-11-27 23:15:22 +0000

    Elastic search implemented functional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,13 @@ class HomeController < ApplicationController
   	@events = Event.where(approved: true).order('created_at DESC').limit(3)
   end
 
-  def search
+def search
+  @query = params[:search]
   if params[:search].nil?
     @result = []
   else
-    @result = Listing.search params[:search]
+    @result = Listing.search(params[:search]).records
   end
-  end
+end
 
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 require 'elasticsearch/model'
+
 class Listing < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
@@ -11,8 +12,6 @@ class Listing < ActiveRecord::Base
   validates :franchisename, :website, uniqueness: true
   validates :fullname, :jobtitle, :email, :franchisename, :leadrecepient, :website, length: { minimum: 5, maximum: 80 }
   validates :branchcount, numericality: { only_integer: true }
-
-end
 
 def self.search(query)
   __elasticsearch__.search(
@@ -27,4 +26,15 @@ def self.search(query)
   )
 end
 
-Listing.import # for auto sync model with elastic search
+end
+
+# Delete the previous articles index in Elasticsearch
+Listing.__elasticsearch__.client.indices.delete index: Listing.index_name rescue nil
+ 
+# Create the new index with the new mapping
+Listing.__elasticsearch__.client.indices.create \
+  index: Listing.index_name,
+  body: { settings: Listing.settings.to_hash, mappings: Listing.mappings.to_hash }
+ 
+# Index all article records from the DB to Elasticsearch
+Listing.import
