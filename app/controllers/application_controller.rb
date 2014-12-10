27a223e91f309460fea50e9efa160d709ddb8c26
cjshort <callumshorty@hotmail.com>
2014-12-10 12:38:46 +0000

    social integration + colouring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	before_filter :spotlightnav
+
+	def spotlightnav
+	  @spotlightnav = Listing.where(approved: true, spotlight: true).order('created_at DESC').limit(2)
+	end
 
   def industrylist
 	  @industrylist = [["Accounting", "Accounting"], ["Agriculture", "Agriculture"], ["Automotive", "Automotive"], ["Beverage","Beverage"], ["B2B","B2B"], ["Cafe And Coffee","Cafe And Coffee"], ["Care","Care"], ["Chemical","Chemical"], ["Children And Babies","Children And Babies"], ["Cleaning","Cleaning"], ["Communications","Communications"], ["Computers","Computers"], ["Consultancy","Consultancy"], ["Logistics","Logistics"], ["Education And Training","Education And Training"], ["Electrical","Electrical"], ["Entertainment","Entertainment"], ["Enviromental","Enviromental"], ["Estate Agents","Estate Agents"], ["Fashion","Fashion"], ["Financial","Financial"], ["Food","Food"], ["Health","Health"], ["Hire And Rental","Hire And Rental"], ["Hospitality","Hospitality"], ["Indoor Property Services","Indoor Property Services"], ["Outdoor Property Services","Outdoor Property Services"], ["Internet","Internet"], ["Legal","Legal"], ["Manafacture","Manafacture"], ["Pets","Pets"], ["Promotional","Promotional"], ["Print","Print"], ["Repair And Maintenance","Repair And Maintenance"]]
