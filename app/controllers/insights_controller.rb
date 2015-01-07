f624dca45d7ab107d6a381b25b92ccb05bb5507a
cjshort <callumshorty@hotmail.com>
2015-01-07 09:58:23 +0000

    insights

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,10 +1,14 @@
 class InsightsController < ApplicationController
-	before_action :authenticate_franchise!
+	#before_action :authenticate_franchise!
 
   def dashboard
+  	@listing = Listing.where(franchise_id: current_franchise.id)
+  	@leads = Lead.where(listing_id: @listing.id)
   end
 
   def leads
+  	@listing = Listing.where(franchise_id: current_franchise.id)
+  	@leads = Lead.where(listing_id: @listing.id)
   end
 
   def editlisting
