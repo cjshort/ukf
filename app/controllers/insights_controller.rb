e29eb3d4a2926e03736e5098a2d966bd9ba1141b
cjshort <callumshorty@hotmail.com>
2015-01-07 11:09:30 +0000

    insights fix

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -2,13 +2,13 @@ class InsightsController < ApplicationController
 	before_action :authenticate_franchise!
 
   def dashboard
-  	@listing = Listing.where(franchise_id: current_franchise.id)
+  	@listing = Listing.find_by(franchise_id: current_franchise.id)
   	@leads = Lead.where(listing_id: @listing.id)
   end
 
   def leads
-  	@listing = Listing.where(franchise_id: current_franchise.id)
-  	@leads = Lead.where(listing_id: @listing.id)
+  	@listing = Listing.find_by(franchise_id: current_franchise.id)
+		@leads = Lead.where(listing_id: @listing.id)
   end
 
   def editlisting
