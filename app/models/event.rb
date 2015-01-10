d12d332631a2ddd64a5f1c55d96b7e3cd8d9ea9a
cjshort <callumshorty@hotmail.com>
2015-01-10 12:47:09 +0000

    remove event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,2 @@
 class Event < ActiveRecord::Base
-  validates_acceptance_of :toc, :on => :create, :accept => true
-  validates :eventname, :startdate, :enddate, :shortdescription, :region, :postcode, :website, presence: true
-  validates :shortdescription, length: { minimum: 50, maximum: 500}
-  validates :eventname, :startdate, :enddate, :region, :postcode, :website, length: { minimum: 3, maximum: 80 }
 end
