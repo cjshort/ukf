01214aa32318ba35b10e710d4e9f566fe364d311
cjshort <callumshorty@hotmail.com>
2015-02-12 20:55:22 +0000

    test4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,6 @@ class PostsController < ApplicationController
     @post2 = Post.where(:approved => true).offset(1).last
     @post3 = Post.where(:approved => true).offset(2).last
     @post4 = Post.where(:approved => true).offset(3).last
-    @postlist = Post.offset(4).where(:approved => true)
   end
 
   def subscribe
